1bb63b863901e99b2ec063275dbdd970c795b4c5
yolixtly <yolixtly_13@hotmail.com>
2017-01-06 15:05:07 -0500

    node modules practice

---
+++ modules/app.js
@@ -0,0 +1,6 @@
+var math = require('./calculator');
+
+console.log(math.add(1,2));
+console.log(math.substract(1,2));
+console.log(math.multiply(1,2));
+console.log(math.divide(1,2));
---
+++ modules/calculator.js
@@ -0,0 +1,17 @@
+var math = {
+  add: function(a,b){
+    // console.log(a,b);
+    return a + b;
+  },
+  substract: function(a,b){
+    return a - b;
+  },
+  multiply: function(a,b){
+    return a * b;
+  },
+  divide: function(a,b){
+    return a / b;
+  }
+};
+
+module.exports = math;
